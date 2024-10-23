88d4deb4ad744ed49aba433297665d08e6a402dd
morgadows123 <morgadows1@yahoo.com>
2024-10-23 11:18:55 -0300

    tela login e materia

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:oteste/pages/tela_login.dart';
+import 'package:oteste/pages/tela_materia.dart';
+
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: TelaLogin(),
+    )
+  );
+}
+
